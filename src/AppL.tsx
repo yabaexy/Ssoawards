3b8199b71a23ae01f0526ac1dead81787507a943
yabaexy <yabaexy@hotmail.com>
2026-04-15 23:13:48 +0900

    error fix

--- src/AppL.tsx
+++ src/AppL.tsx
@@ -4,7 +4,7 @@
  */
 
 import { useState, useEffect } from "react";
-import { motion, AnimatePresence } from "framer-motion";
+import { motion, AnimatePresence } from "motion/react";
 import { 
   Trophy, 
   Cpu, 
